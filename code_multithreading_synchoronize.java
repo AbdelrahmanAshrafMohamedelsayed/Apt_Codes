bc28aa08727631e8f024ef652933eb2cdd750f25
abdoashraf <97232730+AbdelrahmanAshrafMohamedelsayed@users.noreply.github.com>
2022-05-17 22:26:25 +0200

    Add files via upload

---
+++ code_multithreading_synchoronize.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Main extends Thread {
+    public void run() {
+        Count.getInstance();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 1000; i++) {
+            Thread t = new Main();
+            t.start();
+            t.join();
+        }
+        Count u = Count.getInstance();
+
+        System.out.println(u.counter);
+    }
+}
+
+class Count {
+
+    private static volatile Count instance = null;
+
+    public static int counter = 0;
+
+    private Count() {
+    }
+
+    public static Count getInstance() {
+
+                if (instance == null) {
+
+                    instance = new Count();
+                }
+        return instance;
+    }
+    public synchronized static void increment() {
+        counter++;
+    }
+}
---
+++ lazy_initialization.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String args[])
+    {
+        Count s = Count.getInstance();
+        s.increment();s.increment();s.increment();
+        System.out.println(s.counter); //3
+
+        Count s2 = Count.getInstance();
+        System.out.println(s2.counter); //3 also, because s2 is the same instance as s, just two different variable pointing to the same instance
+
+        String txt;
+        txt = (s.equals(s2)) ? "same":"not same";
+        System.out.println(txt); //same
+    }
+}
+class Count {
+
+    private static Count instance = null;
+
+    public int counter=0;
+
+    // This private constructor is to prevent this object get instantiated more than once.
+    private Count(){}
+
+    public static Count getInstance()
+    {
+        if (instance==null){
+            instance =new Count();
+        }
+        return instance;
+    }
+
+    public void increment()
+    {
+        this.counter++;
+    }
+
+
+}
